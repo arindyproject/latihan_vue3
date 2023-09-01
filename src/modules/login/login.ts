5bdb921466bd86f708c292386359b14085eb17e9
arindyproject <aji.arindy@gmail.com>
2023-09-01 22:38:01 +0700

    dfds

--- src/modules/login/login.ts
+++ src/modules/login/login.ts
@@ -1,40 +1,57 @@
-import { Vue } from 'vue-class-component';
-import {ref} from "vue";
-import { useStore } from 'vuex';
+import {
+    Vue
+} from 'vue-class-component';
+import {
+    reactive
+} from "vue";
+import {
+    useStore
+} from 'vuex';
 import store from '@/store';
 
 
 export default class Login extends Vue {
-    windowHeight  = window.innerHeight;
+    windowHeight = window.innerHeight;
 
 
-   store = useStore();
-   app_base_data = store.state.app_base_data;
+    store = useStore();
+    app_base_data = store.state.app_base_data;
 
+    user = reactive({
+        username: "",
+        password: ""
+    })
+
+
+    //==================================================================================
     async mounted() {
         try {
             const response = await this.$axios.get('app.json');
             console.log('Response:', response.data);
-            if(response.data.status){
+            if (response.data.status) {
                 this.app_base_data = response.data.data;
             }
-          } catch (error) {
+        } catch (error) {
             console.error('Error:', error);
-          }
-        
-        if(this.app_base_data.img_background != ''){
+        }
+        //set backround
+        if (this.app_base_data.img_background != '') {
             document.body.style.backgroundImage = `url('${this.app_base_data.img_background}')`;
         }
-          
+
+    }
+    //==================================================================================
+    submitForm(){
+        console.log(this.user.username);
     }
 
-    
-   
+
+    //==================================================================================
     get gradientStyle() {
         return `linear-gradient(${this.app_base_data.main_theme_color}, ${this.app_base_data.second_theme_color})`;
     }
 
-    get mainColor(){
+    get mainColor() {
         return `${this.app_base_data.main_theme_color}`;
     }
 
